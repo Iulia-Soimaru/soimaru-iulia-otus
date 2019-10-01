ea1f7700c28c307f1cf65dbd958ea5d433db6499
Iulia Soimaru <iuliashoimaru@gmail.com>
2019-09-30 23:27:23 -0700

    add more test cases

--- javascript/javascript-1.js
+++ javascript/javascript-1.js
@@ -12,4 +12,7 @@ const sum = (a) => {
   }; 
 }
 
-console.log(sum(1)(2)(3)(4)(5)());
+console.log(sum(1)() === 1);
+console.log(sum(1)(2)() === 3);
+console.log(sum(1)(2)(3)() === 6);
+console.log(sum(1)(2)(3)(4)(5)() === 15);
