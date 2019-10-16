a5e1a7852e7a8862434ca2a86b6c2b57f7f0ed30
Iulia Soimaru <iuliashoimaru@gmail.com>
2019-10-15 22:58:33 -0700

    add promiseReduce function

---
+++ promises-2.js
@@ -0,0 +1,28 @@
+var fn1 = () => {
+  console.log('fn1')
+  return Promise.resolve(1)
+}
+
+var fn2 = () => new Promise(resolve => {
+  console.log('fn2: ')
+  setTimeout(() => {
+    return resolve(2)
+  }, 1000)
+});
+
+function promiseReduce(asyncFunctions, customReduce, initialValue) {
+   return  asyncFunctions.forEach(async fn => {
+      const result = await fn();
+      return customReduce(result, initialValue);
+    });
+};
+
+promiseReduce(
+  [fn1, fn2],
+  function (memo, value) {
+    console.log('reduce: ', memo * value)
+    return memo * value
+  },
+  1
+)
+.then(console.log)
